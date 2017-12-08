4a241017936368e001e2c7aae9058d67f6cf75a8
qingping.niu <nqp_java@163.com>
2017-12-08 11:29:13 +0800

    锁屏业务操作

--- src/main/java/opeartion/ScreenlockOperate.java
+++ src/main/java/opeartion/ScreenlockOperate.java
@@ -19,6 +19,7 @@ public class ScreenlockOperate extends OperateBase {
     Logger logger = LoggerFactory.getLogger(ScreenlockOperate.class);
 
 
+
     /**
      * 构造方法
      * @param driver
@@ -36,7 +37,11 @@ public class ScreenlockOperate extends OperateBase {
         goSleep(2000);
         driver.pressKeyCode(AndroidKeyCode.KEYCODE_POWER);//电源事件,亮屏
         goSleep(2000);
-        swipeToUp(2,7,2,8);
+        int width = driver.manage().window().getSize().width;
+        int height = driver.manage().window().getSize().height;
+
+        swipeToUp(width / 2, height * 7 / 8, width / 2, height / 8, 500);
+
     }
 
     /**
@@ -61,43 +66,50 @@ public class ScreenlockOperate extends OperateBase {
      *从Pattern切换到Pin
      */
     public boolean setPin(){
-        try {
-            homePage.screenLock.click();
-            drawLockPattern(0,1,2,5,8);//绘制确认图案
-            homePage.pin.click(); //进入Pin码设置
-            drawLockPin(1,2,3,6,9); //输入Pin码
-            drawLockPin(1,2,3,6,9);//确认Pin码
-            goSleep(1000);
-            flag = validationLock(Constant.LOCK_PIN,1,2,3,6,9); //验证密码使用效果
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        homePage.screenLock.click();
+        drawLockPattern(0,1,2,5,8);//绘制确认图案
+        homePage.pin.click(); //进入Pin码设置
+        drawLockPin(1,2,3,6,9); //输入Pin码
+        drawLockPin(1,2,3,6,9);//确认Pin码
+        goSleep(1000);
+        flag = validationLock(Constant.LOCK_PIN,1,2,3,6,9); //验证密码使用效果
         return flag;
+    }
+
+    public boolean setSwipe(){
+        homePage.screenLock.click();
+        drawLockPin(1,2,3,6,9); //输入Pin码
+        homePage.swipe.click();
+        flag = validationLock(Constant.LOCK_SWIPE,null); //验证密码使用效果
+
+
+
+        return false;
     }
 
     /**
      * 验证密码效果
      * @param lockType
-     * @param mnuber
+     * @param number
      */
-    public boolean validationLock(int lockType,int...mnuber){
-        try {
-            driver.pressKeyCode(AndroidKeyCode.KEYCODE_POWER); //电源事件,熄屏
-            goSleep(1000);
-            driver.pressKeyCode(AndroidKeyCode.KEYCODE_POWER);//电源事件,亮屏
-            goSleep(1000);
-            swipeToUp(2,7,2,8);
-            if(lockType == Constant.LOCK_PATTERN){
-                drawLockPattern(0,1,2,5,8);//绘制图案
-            }else if(lockType == Constant.LOCK_PIN){
-                drawLockPin(1,2,3,6,9);//确认密码
-            }
-            goSleep(2000);
-            if(driver.getPageSource().contains("com.tcl.joylockscreen:id/unlockview")){//是否包含该id
-                return false; //表示解锁失败
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public boolean validationLock(int lockType,int...number){
+
+        driver.pressKeyCode(AndroidKeyCode.KEYCODE_POWER); //电源事件,熄屏
+        goSleep(1000);
+        driver.pressKeyCode(AndroidKeyCode.KEYCODE_POWER);//电源事件,亮屏
+        goSleep(1000);
+        int width = driver.manage().window().getSize().width;
+        int height = driver.manage().window().getSize().height;
+        swipeToUp(width / 2, height * 7 / 8, width / 2, height / 8, 500);
+        if(lockType == Constant.LOCK_PATTERN){
+            drawLockPattern(number);//绘制图案
+        }else if(lockType == Constant.LOCK_PIN){
+            drawLockPin(number);//确认密码
+        }
+        goSleep(1000);
+        if(driver.getPageSource().contains("com.tcl.joylockscreen:id/unlockview")){//是否包含该id
+            return false; //表示解锁失败
         }
         return true;
     }
