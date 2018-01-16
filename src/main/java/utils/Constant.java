da610d13b85efee029ac54f1464da2acf6554c6a
qingping.niu <nqp_java@163.com>
2018-01-16 17:26:14 +0800

    增加常量

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -10,4 +10,10 @@ public interface Constant {
     public static final int LOCK_SWIPE =1;
 
     public static final String IMG_PATH = "test-output/image";
+
+    public static final int [] PATTERN_NUMBER= {0,1,2,5,8};
+    public static int [] PIN_NUMBER = {1,2,3,6};
+
+//    public static int APP_NOT_ALLSELECT_SWITCH = 0; //未全选
+//    public static int APP_ALLSELECT_SWITCH = 1; //已全选
 }
